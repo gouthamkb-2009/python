564ace7d19d3a038b1b1862011d43e67409592d7
gouthamkb-2009 <gouthamb.24.beai@acharya.ac.in>
2025-05-26 18:26:09 +0530

    Add files via upload

---
+++ CommaCode.py
@@ -0,0 +1,8 @@
+print("Goutham KB ")
+print("1AY24AI035 ")
+print("M ")
+items = ['apples', 'bananas', 'tofu', 'cats']
+if len(items) == 0:
+    print("Empty list")
+else:
+    print(', '.join(items[:-1]) + ', and ' + items[-1])
