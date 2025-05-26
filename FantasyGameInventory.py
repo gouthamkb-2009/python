b1ec9e8e8af729fbbabe8b1152ae9df04afe1a05
gouthamkb-2009 <gouthamb.24.beai@acharya.ac.in>
2025-05-26 18:40:20 +0530

    Add files via upload

---
+++ FantasyGameInventory.py
@@ -0,0 +1,8 @@
+print("Goutham KB ")
+print("1AY24AI035 ")
+print("M ")
+print("Your Name, Your USN, Your Section")
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42}
+print("Inventory:")
+for item, count in inventory.items():
+    print(f"{count} {item}")
