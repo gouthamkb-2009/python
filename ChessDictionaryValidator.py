5fcf50ae19269586f189424638f8b632b99ad5f6
gouthamkb-2009 <gouthamb.24.beai@acharya.ac.in>
2025-05-26 18:35:45 +0530

    Add files via upload

---
+++ ChessDictionaryValidator.py
@@ -0,0 +1,7 @@
+print("Goutham KB ")
+print("1AY24AI035 ")
+print("M ")
+def valid_chess_board(board):
+    # Add validation logic here
+    return True
+print(valid_chess_board({'h1': 'wking'}))  # Example usage
