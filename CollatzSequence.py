37715dbd7001b5cb614a448b3ec5434c80200e56
gouthamkb-2009 <gouthamb.24.beai@acharya.ac.in>
2025-05-26 18:16:40 +0530

    Add files via upload

---
+++ CollatzSequence.py
@@ -0,0 +1,9 @@
+print("Goutham KB ")
+print("1AY24AI035 ")
+print("M ")
+def collatz(number):
+    while number != 1:
+        print(number)
+        number = number // 2 if number % 2 == 0 else 3 * number + 1
+    print(1)
+collatz(int(input("Enter number: ")))
