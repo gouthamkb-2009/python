8e6ca3f7f05b9104886deddf66833e8af918dbf3
gouthamkb-2009 <gouthamb.24.beai@acharya.ac.in>
2025-05-26 18:05:25 +0530

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,14 @@
+print("Goutham KB ")
+print("1AY24AI035 ")
+print("M ")
+import random
+choices = ['rock', 'paper', 'scissors']
+user = input("Enter choice: ").lower()
+comp = random.choice(choices)
+print(f"Computer chose {comp}")
+if user == comp:
+    print("Tie!")
+elif (user == 'rock' and comp == 'scissors') or (user == 'scissors' and comp == 'paper') or (user == 'paper' and comp == 'rock'):
+    print("You win!")
+else:
+    print("You lose!")
